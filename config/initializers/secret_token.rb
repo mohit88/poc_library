61cb33d88edf4033b524784dd70774ece2aacc00
Mohit <mohitsha@thoughtworks.com>
2015-05-06 15:37:35 +0530

    Initialized poc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poc::Application.config.secret_key_base = '3ba88977f6ae72b7305cd73d013c4a371b762fbc4f6b767dd5344e114f40ec192765bf2acb8c30a4c239f075ed4e85aea3c4a88e200736a48ca7675d7003ca70'
