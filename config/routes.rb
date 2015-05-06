533e1a385b750cb9e462178b02ff26a0c47068da
Mohit <mohitsha@thoughtworks.com>
2015-05-06 16:43:27 +0530

    [Kavita/Digs/Mohit] Displaying login page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Poc::Application.routes.draw do
+  resources :login
+  resources :books
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
